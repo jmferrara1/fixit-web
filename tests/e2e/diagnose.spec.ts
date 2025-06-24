ad78961dd8e6b4beddfd74ffa09913858b4b4902
jmferrara1 <129885806+jmferrara1@users.noreply.github.com>
2025-06-24 07:46:47 -0700

    ci: disable all test steps until network is unblocked (#12)

--- tests/e2e/diagnose.spec.ts
+++ tests/e2e/diagnose.spec.ts
@@ -1,2 +1,6 @@
 import { test, expect } from '@playwright/test';
+
+test('diagnose page loads', async ({ page }) => {
+  await page.goto('/diagnose');
+  await expect(page).toHaveURL(/diagnose/);
 });
