365b49b430a2a49f09825164b87c8fb493bdb32e
jmferrara1 <129885806+jmferrara1@users.noreply.github.com>
2025-06-23 21:33:44 -0700

    Update vendor-deps.sh

--- scripts/vendor-deps.sh
+++ scripts/vendor-deps.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 set -e
-# Remove old modules
 
+# Remove any old bits
+rm -rf node_modules/next node_modules/vitest node_modules/.bin/next node_modules/.bin/vitest
+
+# Copy in the vendored packages
+cp -R vendor/next node_modules/next
+cp -R vendor/vitest node_modules/vitest
+
+# Ensure the bin folder exists
+mkdir -p node_modules/.bin
+
+# Copy in the executables
+cp vendor/.bin/next node_modules/.bin/next
+cp vendor/.bin/vitest node_modules/.bin/vitest
+
+# Make sure they’re executable
+chmod +x node_modules/.bin/next
+chmod +x node_modules/.bin/vitest
