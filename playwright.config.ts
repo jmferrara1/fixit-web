a8256b2633742ea0c9c0fb1183e27a49a293c229
jmferrara1 <129885806+jmferrara1@users.noreply.github.com>
2025-06-23 23:19:29 -0700

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,41 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
-export default defineConfig({});
+export default defineConfig({
+  testDir: 'tests/e2e',
+  timeout: 120_000,               // max time for each test
+  expect: {
+    timeout: 10_000,              // max time for expect()
+  },
+  reporter: [['list'], ['html', { open: 'never' }]],
+  use: {
+    baseURL: process.env.BASE_URL || 'http://localhost:3000',
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    actionTimeout: 30_000,
+    ignoreHTTPSErrors: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+  outputDir: 'test-results/',
+  webServer: {
+    command: 'npm run start',
+    url: process.env.BASE_URL || 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120_000,
+  },
+});
